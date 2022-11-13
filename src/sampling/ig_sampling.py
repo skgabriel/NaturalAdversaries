5ec39a74643d573d39fce813f030f122e7fa096b
skgabriel <skgabriel@users.noreply.github.com>
2022-11-13 11:46:29 -0800

    Update ig_sampling.py

--- src/sampling/ig_sampling.py
+++ src/sampling/ig_sampling.py
@@ -27,7 +27,7 @@ def pre_processing(obs, tokenizer, device):
         obs_tensor.to('cpu')
     return obs_tensor
 
-def calculate_outputs_and_gradients(inputs, model, tokenizer, device="cpu"):
+def calculate_outputs_and_gradients(inputs, model, tokenizer, device="cpu", domain="nli"):
 
     #inner function for ig sampling
     def forward_func(inputs):
@@ -43,7 +43,10 @@ def calculate_outputs_and_gradients(inputs, model, tokenizer, device="cpu"):
              input = pre_processing(input, tokenizer, device)
              output = model(input.to(device)).logits
              outputs.append(torch.argmax(torch.nn.functional.softmax(output)))
-             lig = LayerIntegratedGradients(forward_func, model.roberta.embeddings)
+             if args.domain == "nli":
+                 lig = LayerIntegratedGradients(forward_func, model.deberta.embeddings)
+             else:
+                 lig = LayerIntegratedGradients(forward_func, model.roberta.embeddings)
              attributions, error_ = lig.attribute(inputs=input.to(device),baselines=torch.LongTensor([[0] * len(input)]).to(device),return_convergence_delta=True)
              gradients.append(attributions)
     return gradients, outputs
@@ -64,32 +67,44 @@ def main(args):
     batch_size = args.batch_size
     ig_scores = open(args.output_file,"w")
     for i in tqdm.tqdm(range(0,len(file_),batch_size)):
-        try:
-            token_ids = [tokenizer.encode(s[0])[1:-1] for s in file_[i:i+batch_size]]
-        except:
-            continue
-        attr_,outputs =calculate_outputs_and_gradients([s[0] for s in file_[i:i+batch_size]], model, tokenizer,device=device)
+        if args.domain == "nli":
+            try:                                                                                                                                      
+               token_ids = [tokenizer.encode(s[0] + " [SEP] " + s[1])[1:-1] for s in file_[i:i+batch_size]]                                                                  
+            except:                                                                                                                                              
+               continue                                                                                                                                           
+            attr_,outputs =calculate_outputs_and_gradients([s[0] + " [SEP] " + s[1] for s in file_[i:i+batch_size]], model, tokenizer,device=device,domain=args.domain)     
+        else:
+            try:
+               token_ids = [tokenizer.encode(s[0])[1:-1] for s in file_[i:i+batch_size]]
+            except:
+               continue
+            attr_,outputs =calculate_outputs_and_gradients([s[0] for s in file_[i:i+batch_size]], model, tokenizer,device=device,domain=args.domain)
         attr_ = [summarize_attributions(attr_[o]) for o in range(len(attr_))]
-        output_ = [torch.topk(attr_[o],k=int(len(token_ids[o])*args.keep_ratio)) for o in range(len(attr_))]
+        output_ = [torch.topk(attr_[o],k=max(1,int(len(token_ids[o])*args.keep_ratio))) for o in range(len(attr_))]
         for o in range(len(output_)):
             keys = list(set([tokenizer.convert_ids_to_tokens(token_ids[o][int(i)]) for i in output_[o][1].tolist()]))
-            text = [s[0] for s in file_[i:i+batch_size]][o]
-            label = [s[1] for s in file_[i:i+batch_size]][o]
+            if args.domain == "nli":
+                text = [s[0] + " [SEP] " + s[1] for s in file_[i:i+batch_size]][o]                                                                                                  
+                label = [s[2] for s in file_[i:i+batch_size]][o] 
+            else:
+                text = [s[0] for s in file_[i:i+batch_size]][o]
+                label = [s[1] for s in file_[i:i+batch_size]][o]
             split = [args.split for s in file_[i:i+batch_size]][o]
-            line_ = {"text":text,"scores":[float(i) for i in output_[o]],"keys":keys,"label":label,"split":split}
+            import pdb; pdb.set_trace()
+            line_ = {"text":text,"scores":[float(i) for i in output_[o].values],"keys":keys,"label":label,"split":split}
             ig_scores.write(str(json.dumps(line_)) +  "\n") 
-            #ig_scores.writerow([text,output_[o],keys,label,split])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--output_file", type=str, default="ig_scores.jsonl")
-    parser.add_argument("--input_file", type=str, default="example.csv") #in format: example, label
-    parser.add_argument("--model_dir", type=str, default="Xuhui/ToxDect-roberta-large")
+    parser.add_argument("--input_file", type=str, default="nli_example.csv") #in format: example, label
+    parser.add_argument("--model_dir", type=str, default="microsoft/deberta-base-mnli") #default="Xuhui/ToxDect-roberta-large")
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("--split", type=str, default="train")
     parser.add_argument("--keep_ratio", type=float, default=.2)
     parser.add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--domain", type=str, default="nli")
     args = parser.parse_args()
 	
     main(args)
